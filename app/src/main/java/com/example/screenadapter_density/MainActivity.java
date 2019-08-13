abe3bab234d6e1b1412b934ebd40f176ab940fa1
wenchao <860694896@qq.com>
2019-08-13 21:52:56 +0800

    Initial commit

---
+++ app/src/main/java/com/example/screenadapter_density/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.screenadapter_density;
+
+import android.os.Bundle;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
